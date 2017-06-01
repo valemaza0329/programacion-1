fdf7402219d3b69346230a3ce204d71eacc1f040
valemaza0329 <joseteamo0329>
2017-06-01 20:07:01 -0300

    subiendo algunos ejercicios de la pract1

---
+++ pract1/practica 1.py
@@ -0,0 +1,14 @@
+num = 193
+palabra = "la vida es bella"
+print (palabra)
+l = 3 ; m = 2 ; j = 2
+print (l)
+print (m)
+print (j)
+cadena = "Estoy aprendiendo paython"
+print (cadena)
+
+parrafo = """ En Python es posible
+armar un parrafo
+muy sencillamente """
+print (parrafo)
